97471090628c2137863ce5be5675581010e68dd2
lizhuo <lizhuo@yy.com>
2020-02-29 18:33:12 +0800

    socket server

---
+++ 02-socket/src/main/java/com/pear/server/PearHandler.java
@@ -0,0 +1,32 @@
+package com.pear.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author lizhuo
+ * @Description: 自定义服务端处理器
+ * 需求：实现处理 C/S 模式的 socket 通信 demo
+ * @date 2020-02-29 18:21
+ */
+public class PearHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		// 客户端数据 输出只 console
+		System.out.println("client " + ctx.channel().remoteAddress() + " msg: " + msg);
+
+		// 发送给客户端数据
+		ctx.writeAndFlush("from server: " + UUID.randomUUID());
+		TimeUnit.SECONDS.sleep(500);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		ctx.channel().close();
+	}
+
+}
--- 02-socket/src/main/java/com/pear/server/PearServer.java
+++ 02-socket/src/main/java/com/pear/server/PearServer.java
@@ -1,10 +1,53 @@
 package com.pear.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
 /**
  * @author lizhuo
  * @Description: netty server demo
  * @date 2020-02-29 17:52
  */
 public class PearServer {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		EventLoopGroup parentGroup = new NioEventLoopGroup();
+		EventLoopGroup clientGroup = new NioEventLoopGroup();
+
+		try {
+			ServerBootstrap bootstrap = new ServerBootstrap();
+			bootstrap.group(parentGroup, clientGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							ChannelPipeline pipeline = ch.pipeline();
+							// StringDecoder 字符串解码器：将 channel 中的 ByteBuf 数据解码为 String
+							pipeline.addLast(new StringDecoder());
+							// StringEncoder 自负床编码器：将 String 编码 为ByteBuf 发送到 Channel 中
+							pipeline.addLast(new StringEncoder());
+
+							pipeline.addLast(new PearHandler());
+						}
+
+					});
+
+			ChannelFuture future = bootstrap.bind(8888).sync();
+			System.out.println("Server started listening 8888");
+
+			future.channel().closeFuture().sync();
+		} finally {
+			parentGroup.shutdownGracefully();
+			clientGroup.shutdownGracefully();
+			System.out.println("Server shutdown");
+		}
+	}
 
 }
